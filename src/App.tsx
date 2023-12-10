fdef2079f7073d6dbb7c1d3551957385060be92c
Dunridge <Polinka7max@gmail.com>
2023-12-09 20:17:13 -0800

    add-threejs. add threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import { Model } from "./components/Model";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Model/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,18 +1,14 @@
 import * as THREE from 'three';
 import { useEffect, useRef } from "react";
 
-// TODO: rename the component to a more general name 
 export function Model() { 
     const refContainer = useRef(null);
 
     useEffect(() => {
-        // === THREE.JS CODE START ===
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         var renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
-        // document.body.appendChild( renderer.domElement );
-        // use ref as a mount point of the Three.js scene instead of the document.body
         //@ts-ignore
         refContainer.current && refContainer.current?.appendChild(renderer.domElement);
         var geometry = new THREE.BoxGeometry(1, 1, 1);
