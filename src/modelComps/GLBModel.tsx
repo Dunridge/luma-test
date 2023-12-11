3fdedaa0475ad087eceeb7987ad81110e44549dc
Dunridge <Polinka7max@gmail.com>
2023-12-10 18:15:31 -0800

    add-threejs. try different model

--- src/modelComps/GLBModel.tsx
+++ src/modelComps/GLBModel.tsx
@@ -1,7 +1,9 @@
 import { useGLTF, useAnimations } from "@react-three/drei";
 
 const GLBModel = () => {
+    // TODO: configure the settings for inividual models here 
     const model = useGLTF('./mc_laren.glb');
+    // const model = useGLTF('./mercedes.glb');
 
     return (<>
         <mesh>
