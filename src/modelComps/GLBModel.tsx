340aad10dd5d649f4869ff15562fe476760d99cb
Dunridge <Polinka7max@gmail.com>
2023-12-10 22:08:56 -0800

    change-model. add lambo

--- src/modelComps/GLBModel.tsx
+++ src/modelComps/GLBModel.tsx
@@ -1,10 +1,9 @@
 import { useGLTF, useAnimations } from "@react-three/drei";
 
 const GLBModel = () => {
-    // TODO: configure the settings for inividual models here 
-    const model = useGLTF('./mc_laren.glb');
-    // const model = useGLTF('./mercedes.glb');
-
+    // TODO: change lambo-2 to lambo 
+    const model = useGLTF('./lambo-2.glb');
+    
     return (<>
         <mesh>
             <primitive object={model.scene}/>
