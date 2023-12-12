fe8018a9fdc6bf65c4a99af64f5f3a0bc8419df2
Dunridge <Polinka7max@gmail.com>
2023-12-11 16:31:00 -0800

    define-sections. add form

--- src/components/ContactUsInput.tsx
+++ src/components/ContactUsInput.tsx
@@ -3,7 +3,7 @@ import { IContactUsInput } from "../utils/interfaces/IContactUsInput";
 export default function ContactUsInput({ name, value, handleChange }: IContactUsInput) {
 
     return (
-        <label className="contact-us__input">
+        <label className="contact-us-input">
             <span>{name}: </span>
             <input
                 type="text"
