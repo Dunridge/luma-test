20233cf7cb67faa9cdc3d6fabfe8b637adec6513
Dunridge <Polinka7max@gmail.com>
2023-12-12 11:10:15 -0800

    define-sections. fix background

--- src/modelComps/Scene.tsx
+++ src/modelComps/Scene.tsx
@@ -21,11 +21,7 @@ export const Scene = () => {
 
     return (<>
         <color attach='background' />
-        <Environment preset="city" background />
-        {/* <Environment preset="studio" background /> */}
-        {/* TODO: set your custom hdr background if you figure out why it's not showing */}
-        {/* <Environment files='puresky.hdr'/> */}
-        {/* <Environment files='src/assets/preset/puresky.hdr'/> */}
+        <Environment files='puresky.hdr' background/>
         <PerspectiveCamera theatreKey="Camera" makeDefault position={[2, 0, 0]} fov={90} near={0.1} far={70} />
         <ambientLight intensity={10} />
         <directionalLight intensity={3} />
