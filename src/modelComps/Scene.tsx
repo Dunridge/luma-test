dbd8b961fb448baf334ec4962ef9f13db30494a0
Dunridge <Polinka7max@gmail.com>
2023-12-12 12:31:04 -0800

    define-sections. add rotations

--- src/modelComps/Scene.tsx
+++ src/modelComps/Scene.tsx
@@ -1,4 +1,4 @@
-import { Environment, useScroll } from "@react-three/drei";
+import { Environment, useGLTF, useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { val } from "@theatre/core";
 import {
@@ -6,10 +6,26 @@ import {
     useCurrentSheet
 } from "@theatre/r3f";
 import GLBModel from "./GLBModel";
+import React, { useEffect, useRef } from "react";
+import { MathUtils } from "three";
 
 export const Scene = () => {
     const sheet = useCurrentSheet();
     const scroll = useScroll();
+    const { scene, nodes, animations } = useGLTF("/lambo-2.glb");
+
+    const initialRotation = { x: 0, y: 0, z: 0 };
+    const currentRotation = useRef(initialRotation);
+
+    const updateRotation = () => {
+        // Adjust the rotation based on scroll offset
+        currentRotation.current.y = scroll.offset * Math.PI * 2;
+    };
+
+    useEffect(() => {
+        updateRotation();
+        console.log('scroll.offset', scroll.offset);
+    }, [scroll.offset]);
 
     useFrame(() => {
         //@ts-ignore
@@ -17,11 +33,16 @@ export const Scene = () => {
         //@ts-ignore
         sheet.sequence.position = scroll.offset * sequenceLength;
 
+        scene.rotation.y = MathUtils.lerp(
+            scene.rotation.y,
+            currentRotation.current.y,
+            0.1
+          );
     })
 
     return (<>
         <color attach='background' />
-        <Environment files='puresky.hdr' background/>
+        <Environment files='puresky.hdr' background />
         <PerspectiveCamera theatreKey="Camera" makeDefault position={[2, 0, 0]} fov={90} near={0.1} far={70} />
         <ambientLight intensity={10} />
         <directionalLight intensity={3} />
