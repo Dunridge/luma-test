77b0cffb4612d8fcb43a59e7193c9a6de233b174
Dunridge <Polinka7max@gmail.com>
2023-12-11 16:35:59 -0800

    define-sections. define pricing sections

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -5,6 +5,12 @@ export const Pricing = () => {
         <div className="pricing">
             <div className="pricing__title">Pricing</div>
 
+            <div className="pricing__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo, magnam quos consequatur aliquid officia architecto impedit. Cumque nihil, officia quaerat qui eos repellendus, voluptatum, aliquam a id natus eaque.
+                Omnis, ipsum. Et inventore, sequi eligendi neque suscipit rem ratione aperiam non fugiat corporis a quos libero aspernatur odit provident modi soluta maiores saepe asperiores, dolorem iure natus atque? Eos?
+                Consequuntur expedita, blanditiis eveniet impedit excepturi reprehenderit quam magnam, labore culpa suscipit, inventore odio voluptates? Velit maxime eius quasi? Sit excepturi dolores quo laborum nihil, nemo architecto? Iste, atque expedita.
+            </div>
+
             <div className="pricing__prices">$608,358</div>
         </div>
     );
